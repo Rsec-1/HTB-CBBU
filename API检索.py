51ad134b0d82bd2ac457dee5d5eebc02990811fd
Rsec <119555373+Rsec-1@users.noreply.github.com>
2023-10-07 14:18:13 +0800

    Create API检索.py

---
+++ API检索.py
@@ -0,0 +1,15 @@
+import requests, sys
+
+def brute():
+    try:
+        value = range(10000)
+        for val in value:
+            url = sys.argv[1]
+            r = requests.get(url + '/?id='+str(val))
+            if "position" in r.text:
+                print("Number found!", val)
+                print(r.text)
+    except IndexError:
+        print("Enter a URL E.g.: http://<TARGET IP>")
+
+brute()
